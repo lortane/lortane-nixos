85b72766f1b9e68a8e809b34d6d4242d5f076d60
lortane <lortane@pm.me>
2025-09-18 16:32:15 +0200

    Create Jack host

---
+++ hosts/jack/default.nix
@@ -0,0 +1,35 @@
+{
+  outputs,
+  config,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    ./boot.nix
+    ./hardware.nix
+    ./networking.nix
+
+    ../../users/lortane
+
+    outputs.nixosModules.common
+  ];
+
+  # So I can deploy remotely (review if can be done better)
+  security.sudo.wheelNeedsPassword = false;
+
+  # Enable SSH
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "prohibit-password";
+      KbdInteractiveAuthentication = false;
+      MaxAuthtries = 3;
+      MaxSessions = 3;
+    };
+  };
+
+  system.stateVersion = "25.05";
+}
---
+++ hosts/jack/networking.nix
@@ -0,0 +1,19 @@
+{
+  networking.hostName = "boris";
+
+  # Static IP configuration
+  networking.interfaces.eno1.ipv4.addresses = [
+    {
+      address = "192.168.1.135";
+      prefixLength = 24;
+    }
+  ];
+
+  networking.defaultGateway = "192.168.1.1";
+  networking.nameservers = [
+    "192.168.1.1"
+    "1.1.1.1"
+    "8.8.8.8"
+  ];
+  networking.resolvconf.enable = true;
+}
