a54fd7aed958cdd9f34caf9e79f72a88c8e48459
lortane <lortane@pm.me>
2025-10-02 20:58:55 +0200

    Move awesome rc.lua file

--- modules/home/awesomewm/default.nix
+++ modules/home/awesomewm/default.nix
@@ -1,12 +1,8 @@
 { config, pkgs, ... }:
 
-let
-  # Check if development config exists, otherwise use embedded
-  devConfigPath = "/home/lortane/.config/awesome-dev/rc.lua";
-in
 {
   # This creates an editable file
-  home.file.".config/awesome/rc.lua".source = config.lib.file.mkOutOfStoreSymlink devConfigPath;
+  home.file.".config/awesome/rc.lua".source = ./rc.lua;
 
   home.packages = with pkgs; [
     rofi
