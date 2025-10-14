323d6cc60e499f28bfc7082cff941e830709e86a
lortane <lortane@pm.me>
2025-10-14 14:12:57 +0200

    Failed attempt at using tuigreet

--- modules/nixos/desktop/wm/i3/default.nix
+++ modules/nixos/desktop/wm/i3/default.nix
@@ -9,6 +9,31 @@
   enableFor = config: config.desktop.windowManager == "i3";
 in {
   config = mkIf (enableFor config) {
+    environment.systemPackages = with pkgs; [
+      tuigreet
+      dbus
+      xorg.xorgserver
+      xorg.xinit
+      xorg.xauth
+    ];
+
+    environment.etc."greetd/sessions/i3-x11.sh".text = ''
+      #!/bin/sh
+
+      export XDG_SESSION_TYPE=x11
+
+      # ensure we run i3 under a per-session DBus (notifications, systemd user units, etc.)
+      dbus-run-session xfce+i3
+    '';
+
+    environment.etc."greetd/sessions/i3.desktop".text = ''
+      [Desktop Entry]
+      Name=i3 (X11)
+      Exec=/etc/greetd/sessions/i3-x11.sh
+      Type=Application
+      NoTerminal=true
+    '';
+
     services.xserver = {
       enable = true;   
       desktopManager = {
@@ -20,12 +45,13 @@ in {
         };
       };
       windowManager.i3.enable = true;
-      displayManager.lightdm = {
-        enable = true;
-        greeters.gtk
-    
-      };
+      displayManager.lightdm.enable = false;
     };
-    services.displayManager.defaultSession = "xfce+i3";
+    services.greetd = {
+      enable = true;
+      settings.default_session.command = "${pkgs.tuigreet}/bin/tuigreet --sessions /etc/greetd/sessions";
+    };
+
+    # services.displayManager.defaultSession = "xfce+i3";
   };
 }
